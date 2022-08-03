f0ff66d04caa1a1942405f1f470e38681085e92c
Kurt Timajo <djkurttimajo@gmail.com>
2022-08-04 07:31:25 +0800

    resource method naming, using layout components

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -3,18 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\Listing;
-use Illuminate\Http\Request;
+// use Illuminate\Http\Request;
 
 class ListingController extends Controller
 {
     public function index(){
-        return view('listings',[
+        return view('listings.index',[
             'listings' => Listing::all()
         ]);
     }
 
     public function show(Listing $listing){
-        return view('listing', [
+        return view('listings.show', [
             'listing' => $listing
         ]);
     }
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -48,7 +48,7 @@
 
     {{-- VIEW OUTPUT --}}
     <main>
-        @yield('content')
+        {{ $slot }}
     </main>
 
     <footer
@@ -59,8 +59,8 @@
     <a
         href="create.html"
         class="absolute top-1/3 right-10 bg-black text-white py-2 px-5"
-        >Post Job</a
-    >
+        >Post Job
+    </a>
 </footer>
 </body>
 </html>
