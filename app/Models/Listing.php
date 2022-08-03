b1594bb6f0146919b149412fe5f96c3692827610
Kurt Timajo <djkurttimajo@gmail.com>
2022-08-03 13:14:48 +0800

    db,migration,factory,seeding

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -1,35 +1,11 @@
 <?php
+
 namespace App\Models;
 
-class Listing {
-    public static function all() {
-        return [
-                [
-                    'id' => 1,
-                    'title' => 'Listing One', 
-                    'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-                ],
-                [
-                    'id' => 2,
-                    'title' => 'Listing Two', 
-                    'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-                ],
-                [
-                    'id' => 3,
-                    'title' => 'Listing Three', 
-                    'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-                ]
-            ];
-    }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-
-    public static function find($id){
-        $listings = self::all();
-
-        foreach($listings as $listing){
-            if($listing['id'] == $id){
-                return $listing;
-            }
-        }
-    }
+class Listing extends Model
+{
+    use HasFactory;
 }
