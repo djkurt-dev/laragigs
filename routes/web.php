62e5b3771a0dffbe1a9a27643fe0c0dd376dd0d2
Kurt Timajo <djkurttimajo@gmail.com>
2022-08-03 10:17:12 +0800

    routing

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/hello', function(){
+    return response('<h1>Hello World</h1>',200)
+    ->header('Content-Type','text/plain')
+    ->header('foo','bar');
+});
+
+Route::get('/posts/{id}', function($id){
+    //dd($id); //dump n die
+    return response('Post '.$id);
+})->where('id','[0-9]+');
+
+Route::get('/search', function(Request $request){
+    return $request->name . ' ' . $request->city;
 });
