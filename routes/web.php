dd03001b252ee179d7bf8a63938ccc1dcc2e68ce
Kurt Timajo <djkurttimajo@gmail.com>
2022-08-10 23:18:10 +0800

    auth & middleware

--- routes/web.php
+++ routes/web.php
@@ -18,39 +18,45 @@ use App\Http\Controllers\ListingController;
 */
 
 //all listings
-Route::get('/', [ListingController::class, 'index']);
+Route::get('/', [ListingController::class, 'index'])
+->middleware('auth');
 
 //Show create listing form
-Route::get('/listings/create', [ListingController::class, 'create']);
+Route::get('/listings/create', [ListingController::class, 'create'])
+->middleware('auth');
 
 //Store LIsting data
-Route::post('/listings',[ListingController::class, 'store']);
+Route::post('/listings',[ListingController::class, 'store'])
+->middleware('auth');
 
 //Update listing
-Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit'])
+->middleware('auth');
 
 //edit submit to update
-Route::put('/listings/{listing}',[ListingController::class, 'update']);
+Route::put('/listings/{listing}',[ListingController::class, 'update'])
+->middleware('auth');
 
 //Delete listing
-Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
+Route::delete('/listings/{listing}', [ListingController::class, 'destroy'])
+->middleware('auth');
 
 //single listing //route model binding
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
 
-
-
 //Show Register/Create User Form
-Route::get('/register', [UserController::class, 'create']);
+Route::get('/register', [UserController::class, 'create'])->middleware('guest');
 
 //create new user
 Route::post('/users', [UserController::class, 'store']);
 
 //Log User Out 
-Route::post('/logout', [UserController::class, 'logout']);
+Route::post('/logout', [UserController::class, 'logout'])
+->middleware('auth');
 
 //Show Login Form
-Route::get('/login', [UserController::class, 'login']);
+Route::get('/login', [UserController::class, 'login'])
+->name('login')->middleware('guest');
 
 //Log in User
 Route::post('/users/authenticate',[UserController::class, 'authenticate']);
